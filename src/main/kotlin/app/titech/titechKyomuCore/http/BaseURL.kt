52c410cc5dbc87f4373762e458a1102cf84c015e
nanashiki <nanashiki.app@gmail.com>
2023-08-20 03:04:14 +0900

    Change kyomuWeb URL

--- src/main/kotlin/app/titech/titechKyomuCore/http/BaseURL.kt
+++ src/main/kotlin/app/titech/titechKyomuCore/http/BaseURL.kt
@@ -2,8 +2,8 @@ package app.titech.titechKyomuCore.http
 
 class BaseURL {
     companion object {
-        var host = "kyomu2.gakumu.titech.ac.jp"
-        var origin = "https://kyomu2.gakumu.titech.ac.jp"
+        var host = "kyomu0.gakumu.titech.ac.jp"
+        var origin = "https://kyomu0.gakumu.titech.ac.jp"
 
         fun changeToMock() {
             host = "titech-kyomu-mock.s3.ap-northeast-1.amazonaws.com"
