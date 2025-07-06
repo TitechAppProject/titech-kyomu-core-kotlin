928531a4f3d189b3a27a3c81142577c7c7a23b9b
nanashiki <nanashiki.app@gmail.com>
2025-07-07 05:14:54 +0900

    ver. 4.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "app.titech"
             artifactId = "titech-kyomu-core"
-            version = "4.0.0"
+            version = "4.1.0"
 
             from(components["java"])
 
